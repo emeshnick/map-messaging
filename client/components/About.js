8eb6bcd13dad2c445abbf65b7f4dd48c58ca7875
Elijah Meshnick <emeshnick@gmail.com>
2022-01-30 18:07:04 -0700

    added styles to home and about components

--- client/components/About.js
+++ client/components/About.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
+const styles = {
+  mainContainer: {
+    padding: "5rem",
+  },
+};
 class About extends React.Component {
   render() {
     return (
-      <Container>
+      <Container style={styles.mainContainer}>
         <h1>About</h1>
         <p>Created in 2021</p>
       </Container>
